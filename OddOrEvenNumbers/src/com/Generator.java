570062b55dc272d9ca27ae7f746dd1baa6da18c3
Boyan <boiansinilkov@abv.bg>
2022-07-17 22:32:30 +0300

    developed this program

---
+++ OddOrEvenNumbers/src/com/Generator.java
@@ -0,0 +1,34 @@
+package com;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+
+public class Generator {
+
+	public static void main(String[] args) {
+		String path = "C:\\Users\\PC\\Desktop\\test\\program.txt";
+		File file= new File(path);
+		try {
+			
+			BufferedWriter writer =new BufferedWriter(new FileWriter(path));
+			for (int i = 0; i < 100000; i++) {
+				writer.write(	"\t\t} else if (number == "+(i+1)+") {\r\n" + 
+						"			System.out.println(\"number is odd\");\r\n");
+				i++;
+				writer.write("		} else if (number == "+(i+1)+") {\r\n" + 
+						"			System.out.println(\"number is even\");\r\n" );
+				
+			}
+			writer.close();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+	}
+}
---
+++ OddOrEvenNumbers/src/com/TheProgram.java
@@ -0,0 +1,18 @@
+package com;
+
+public class TheProgram {
+	
+	public static void main(String []args) {
+		int number = 10;
+		
+		if (number == 1) {
+			System.out.println("number is odd");
+		} else if (number == 2) {
+			System.out.println("number is even");
+		} else if (number == 3) {
+			System.out.println("number is odd");
+		} else if (number == 4) {
+			System.out.println("number is even");
+		} 
+	}
+}
